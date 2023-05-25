9af325131c95c9a660dfe94ce4b1c8e838f7c8e6
llopz <almachado@uninorte.edu.co>
2023-05-24 22:12:44 -0500

    Segunda actualizacion de pruebas

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main() -> None:
 import unittest
 'pruebas unitarias'
 
-class RestauranteTest (unittest.TestCase):
+'''class RestauranteTest (unittest.TestCase):
         
     def test_creacion_restaurante(self):
         restaurante = Restaurante('Carlos Restaurant')
@@ -86,16 +86,16 @@ class ItemTest (unittest.TestCase):
         self.assertEqual(patata.cantidad, 1)
 
     # El indice funciona pero como se siguen creando itens antes que este en el main no sabemos cual id se le asignara
-    '''def test_id_item (self):
+    def test_id_item (self):
         patata = Item('Patata', 1)
-        self.assertEqual(patata.id, 2)'''
+        self.assertEqual(patata.id, 2)
     
     # Este si que no sirve, parece que hay algo con el id en los casos de prueba, ya que la variable id no aumenta
-    '''def test_aumento_id (self):
+    def test_aumento_id (self):
         id_anterior = Item.id
         id_esperado = 1 + id_anterior
         patata = Item('Patata', 1)
-        self.assertEqual(Item.id, id_esperado)'''
+        self.assertEqual(Item.id, id_esperado)
     
 class ClienteTest (unittest.TestCase):
 
@@ -107,8 +107,59 @@ class ClienteTest (unittest.TestCase):
         Lopez = Cliente('Luis') 
         self.assertEqual(Lopez.get_nombre(), 'Luis')
 
+class PLatoTest (unittest.TestCase):
+
+    def test_creacion_plato(self):
+        pastas = Plato('Pastas', 'pastas en salsa de tomate', 6000)
+        self.assertIsNotNone(pastas)
+
+    def test_nombre_plato(self):
+        pastas = Plato('Pastas', 'pastas en salsa de tomate', 6000)
+        self.assertEqual(pastas.nombre, 'Pastas')
+
+    def test_descripcion_plato(self):
+        pastas = Plato('Pastas', 'pastas en salsa de tomate', 6000)
+        self.assertEqual(pastas.descripcion, 'pastas en salsa de tomate')
+
+    def test_precio_plato(self):
+        pastas = Plato('Pastas', 'pastas en salsa de tomate', 6000)
+        self.assertEqual(pastas.precio, 6000)
+
+    def test_actualizar_precio_plato(self):
+        pastas = Plato('Pastas', 'pastas en salsa de tomate', 6000)
+        pastas.actualizar_precio(8000)
+        self.assertEqual(pastas.precio, 8000)
+
+class MesaTest (unittest.TestCase) :
+
+    def test_creacion_mesa(self):
+        mesa = Mesa(1, 4)
+        self.assertIsNotNone(mesa)
+
+    def test_numero_mesa(self):
+        mesa = Mesa(1, 4)
+        self.assertEqual(mesa.numero, 1)
+
+    def test_capacidad_mesa(self):
+        mesa = Mesa(1, 4)
+        self.assertEqual(mesa.capacidad, 4)
+
+    def test_estado_por_defecto_mesa(self):
+        mesa = Mesa(1, 4)
+        self.assertFalse(mesa.ocupada)
+
+    def test_ocupar_mesa(self):
+        mesa = Mesa(1, 4)
+        mesa.ocupar()
+        self.assertTrue(mesa.ocupada)
+
+    def test_desocupar_mesa(self):
+        mesa = Mesa(1, 4)
+        mesa.ocupar()
+        mesa.desocupar()
+        self.assertFalse(mesa.ocupada)'''
+
     
 
 if __name__ == '__main__':
     main()
-    unittest.main()
